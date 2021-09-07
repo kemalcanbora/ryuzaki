74d9f921d554f8d781739b4656d8413c460c297e
Kemalcan Bora <kemalcanbora@gmail.com>
2021-09-07 17:28:46 +0300

    added hello page

--- main.go
+++ main.go
@@ -1,19 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/jasonlvhit/gocron"
+	"log"
 	"net/http"
 	route "ryuzaki/app/route"
 )
 
+func Hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World")
+}
+
 func main() {
 	go GetAllGamesInSteamEveryDay()
 	handler := mux.NewRouter()
+	handler.HandleFunc("/", Hello).Methods("GET")
 	handler.HandleFunc("/api/steam", route.HelloSteam).Methods("POST")
 	err := http.ListenAndServe("0.0.0.0:8080", handler)
 	if err != nil {
-		return
+		log.Fatalln("Listen and Serve problem..")
 	}
 }
 
